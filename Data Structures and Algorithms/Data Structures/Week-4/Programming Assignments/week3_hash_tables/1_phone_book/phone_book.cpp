679a863700d6617c7f80efd2ea2236b0eddf896a
Krishna Srikar Durbha <srikarkrishnad@gmail.com>
2020-07-14 23:57:43 +0530

    Week-4 Data Structures

--- Data Structures and Algorithms/Data Structures/Week-4/Programming Assignments/week3_hash_tables/1_phone_book/phone_book.cpp
+++ Data Structures and Algorithms/Data Structures/Week-4/Programming Assignments/week3_hash_tables/1_phone_book/phone_book.cpp
@@ -1,72 +1,64 @@
 #include <iostream>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <cstdlib>
 #include <vector>
+#include <sstream>
+#include <iomanip>
+#include <limits.h>
 #include <string>
+#include <math.h> 
+#include <float.h>
+#include <bitset>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define fr(i, a, b) for (int i=a; i<=b; i++)
+#define vec1d(v,T,n,init) vector<T> v(n,init)
+#define vec2d(v,T,n,m,init) vector<vector<T>> v(n, vector<T>(m,init))
 
-using std::string;
-using std::vector;
-using std::cin;
+// Hash Matrix
+#define Max 10000000
+vec1d(Hash,string,Max,"not found");
 
-struct Query {
-    string type, name;
-    int number;
-};
+string Find(int x){
+    return Hash[x];
+}
 
-vector<Query> read_queries() {
+void Add(int x, string contant){
+    Hash[x] = contant;
+}
+
+void Delete(int x){
+    Hash[x] = "not found";
+}
+
+int main(){
     int n;
     cin >> n;
-    vector<Query> queries(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> queries[i].type;
-        if (queries[i].type == "add")
-            cin >> queries[i].number >> queries[i].name;
-        else
-            cin >> queries[i].number;
-    }
-    return queries;
-}
-
-void write_responses(const vector<string>& result) {
-    for (size_t i = 0; i < result.size(); ++i)
-        std::cout << result[i] << "\n";
-}
-
-vector<string> process_queries(const vector<Query>& queries) {
-    vector<string> result;
-    // Keep list of all existing (i.e. not deleted yet) contacts.
-    vector<Query> contacts;
-    for (size_t i = 0; i < queries.size(); ++i)
-        if (queries[i].type == "add") {
-            bool was_founded = false;
-            // if we already have contact with such number,
-            // we should rewrite contact's name
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts[j].name = queries[i].name;
-                    was_founded = true;
-                    break;
-                }
-            // otherwise, just add it
-            if (!was_founded)
-                contacts.push_back(queries[i]);
-        } else if (queries[i].type == "del") {
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts.erase(contacts.begin() + j);
-                    break;
-                }
-        } else {
-            string response = "not found";
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    response = contacts[j].name;
-                    break;
-                }
-            result.push_back(response);
+    
+    while(n--){
+        string operation;
+        cin >> operation;
+        
+        if (operation == "add"){
+            int number;
+            string name;
+            cin >> number >> name;
+            Add(number,name);
         }
-    return result;
-}
 
-int main() {
-    write_responses(process_queries(read_queries()));
-    return 0;
+        else if (operation == "find"){
+            int number;
+            cin >> number;
+            cout << Find(number) << endl;
+        }
+
+        else{
+            int number;
+            cin >> number;
+            Delete(number);
+        }
+    }
 }
